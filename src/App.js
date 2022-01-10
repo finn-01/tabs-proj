8b523ee8f35861447c9d3908df93f34743fc725a
finn-01 <nguyensam576@gmail.com>
2022-01-10 19:02:52 +0700

    setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const url = "https://course-api.com/react-tabs-project";
+const App = () => {
+	return <div>Hello App</div>;
+};
 
 export default App;
